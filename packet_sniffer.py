a7ad585e408505d7f6e50d56ebcf8f18464bc632
ifejika chichetam('kidiIT') <kidi@DESKTOP-8EITQU7.localdomain>
2024-10-25 15:58:22 +0100

    Update modified files only

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,10 +1,11 @@
 import subprocess
 from scapy.all import sniff
-tcpdump_process = subprocess.Popen(['tcpdump', '-i','eth0', '-w', 'capture.pcap'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+tcpdump_process = subprocess.Popen(['tcpdump', '-i','eth0', '-w', 'capture.pcap'],stdout=subprocess.PIPE,stderr=subprocess.PIPE) 
 # packet sniffing processing logic
 def packet_callback(packet):
-    print(packet.summary())
+    print(packet.summary())     
 def start_sniffing(interface=None):
-    sniff(iface=interface,prn=packet_callback, store=False)
+    sniff(iface=interface, prn=packet_callback, store=False)
 start_sniffing()
 tcpdump_process.terminate()
+ 
